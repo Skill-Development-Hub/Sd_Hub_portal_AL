3ab8da6aae0a0505ed9d560888f57d0ba82d06ca
AbdulLateefPasha <lateefpasha1777@gmail.com>
2024-10-03 00:10:32 -0700

    Fix

--- sdhub-portal/src/app/app-routing.module.ts
+++ sdhub-portal/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 
-
 const routes: Routes = [
   { path: '', redirectTo: 'signin', pathMatch: "full" },
   { path: 'signin', component: SigninComponent },
