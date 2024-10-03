62d6fbdd8bd261a9abef5d6733ec1d990ba63e48
AbdulLateefPasha <lateefpasha1777@gmail.com>
2024-10-02 23:34:00 -0700

    Signin And Material
    Generated Signin Component
    Install Material

--- sdhub-portal/src/app/app.module.ts
+++ sdhub-portal/src/app/app.module.ts
@@ -3,17 +3,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
